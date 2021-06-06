7523c0b4dbc7abb84d8f6c0738564b1a8105c698
EinSteini <fanokopterhd@gmail.com>
2021-06-06 17:03:19 +0200

    different parameters

--- main.py
+++ main.py
@@ -13,21 +13,21 @@ class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
-    def on_L3_up(self):
+    def on_L3_up(self, a):
         print("W")
         write_report(NULL_CHAR*2+chr(26)+NULL_CHAR*5)
 
-    def on_L3_down(self):
+    def on_L3_down(self, a):
         print("S")
         write_report(NULL_CHAR*2+chr(22)+NULL_CHAR*5)
 
-    def on_L3_left(self):
+    def on_L3_left(self, a):
         print("A")
         write_report(NULL_CHAR*2+chr(4)+NULL_CHAR*5)
 
-    def on_L3_right(self):
+    def on_L3_right(self, a):
         print("D")
         write_report(NULL_CHAR*2+chr(7)+NULL_CHAR*5)
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
-controller.listen()
+controller.listen(timeout=60)
