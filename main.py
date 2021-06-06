f88668e081169ff0c0990b230a4e6e1db4aae269
Your Name <you@example.com>
2021-06-06 15:30:22 +0200

    added main file

---
+++ main.py
@@ -0,0 +1,19 @@
+from pyPS4Controller.controller import Controller
+
+
+NULL_CHAR = chr(0)
+
+def write_report(report):
+    with open('/dev/hidg0', 'rb+') as fd:
+        fd.write(report.encode())
+
+class MyController(Controller):
+
+    def __init__(self, **kwargs):
+        Controller.__init__(self, **kwargs)
+
+    def on_L3_up(self):
+        print("W")
+        write_report(NULL_CHAR*2+chr(17)+NULL_CHAR*5)
+
+controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
