c2695865ac7172e5cbe144f459bf2c3a7b7d98d5
Your Name <you@example.com>
2021-06-18 18:00:25 +0200

    changed behoavior

--- testct.py
+++ testct.py
@@ -6,6 +6,12 @@ class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
+    def on_L2_press(self, a):
+        print("press")
+
+    def on_L2_release(self):
+        print("release")
+
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 controller.listen()
